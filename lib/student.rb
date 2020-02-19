9cfeb02ebf1970e31c38c9408dfa1f86dad65b69
David Van Fleet <dvfleet413@gmail.com>
2020-02-19 20:37:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,5 +32,9 @@ class Student
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) VALUES (?, ?);
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    
+  end 
   
 end

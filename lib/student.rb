db51b508a0cfe3ab2a084580f2ab28de6857fd39
David Van Fleet <dvfleet413@gmail.com>
2020-02-19 20:32:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end 
 
   def self.create_table
     sql = <<-SQL
